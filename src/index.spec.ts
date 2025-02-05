8ba470b241d38ad65eb0bc70466feb3ff4983043
Jonathan Fierro <jotafierro.tech@gmail.com>
2025-02-05 12:08:18 -0300

    feat: add subtract, multiply and divide functions and its respective unit tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,25 @@
-import { sum } from 'src/index';
+import { sum, subtract, multiply, divide } from './';
 
 describe('sum', () => {
   it('should to add two numbers correctly', () => {
     expect(sum(1, 2)).toBe(3);
   });
 });
+
+describe('substract', () => {
+  it('should to subtract two numbers correctly', () => {
+    expect(subtract(2, 1)).toBe(1);
+  });
+})
+
+describe('multiply', () => {
+  it('should to multiply two numbers correctly', () => {
+    expect(multiply(2, 2)).toBe(4);
+  });
+})
+
+describe('divide', () => {
+  it('should to divide two numbers correctly', () => {
+    expect(divide(4, 2)).toBe(2);
+  });
+})
